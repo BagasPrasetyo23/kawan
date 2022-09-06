cecaf4cf2ed8dab58221fa4a6b8380aa7fb015af
mrbm23 <mrbm23962@gmail.com>
2022-09-06 15:25:56 +0700

    penambahan file

---
+++ app/src/main/java/com/project/kawan/More_Hightlight_Kuliner_Activity.kt
@@ -0,0 +1,11 @@
+package com.project.kawan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class More_Hightlight_Kuliner_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_hightlight_kuliner)
+    }
+}
