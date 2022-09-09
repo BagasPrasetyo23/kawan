5e5fd1390edf987bd8e2de38c9a11633aeb34db1
mrbm23 <mrbm23962@gmail.com>
2022-09-09 13:00:56 +0700

    penambahan file

--- app/src/main/java/com/project/kawan/RegisterActivity.kt
+++ app/src/main/java/com/project/kawan/RegisterActivity.kt
@@ -4,27 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 
 class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        val back : ImageView = findViewById(R.id.back)
+        val back : Button = findViewById(R.id.back)
         back.setOnClickListener(this)
-        val tv_to_login: TextView = findViewById(R.id.tv_to_login)
-        tv_to_login.setOnClickListener(this)
     }
     override fun onClick(v: View?) {
         if (v !=null){
             when (v.id){
-                R.id.tv_to_login ->{
-                    val pindahIntent = Intent(this,LoginActivity::class.java)
-                    startActivity(pindahIntent)
-                }
                 R.id.back ->{
                     val pindahIntent = Intent(this,LoginActivity::class.java)
                     startActivity(pindahIntent)
